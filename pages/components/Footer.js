ed189ec8bc2839c4dedec64cd381ee13d98bd553
leo5imon <leo.simvn@gmail.com>
2024-07-29 22:25:16 +0200

    fix dark mode

--- pages/components/Footer.js
+++ pages/components/Footer.js
@@ -4,7 +4,7 @@ import styles from "./Footer.module.css";
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <div className={styles.container}>
+      <div className={styles.innerContainer}>
         <div className={styles.content}>
           <span className={styles.text}>
             By{" "}
