4c5930f05ec0bffd54507cae4b4db98e9bcce799
leo5imon <leo.simvn@gmail.com>
2024-07-31 12:45:32 +0200

    cast meme

--- pages/farcaster.js
+++ pages/farcaster.js
@@ -6,64 +6,6 @@ import styles from "./index.module.css";
 export default function Farcaster() {
   return (
     <>
-      <Head>
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="favicon-16x16.png"
-        />
-        <link rel="manifest" href="site.webmanifest" />
-        <link rel="mask-icon" href="safari-pinned-tab.svg" color="#5bbad5" />
-        <meta name="msapplication-TileColor" content="#da532c" />
-        <meta name="theme-color" content="#ffffff" />
-
-        <title>Farcaster Memes Generator</title>
-        <meta name="description" content="Generate memes based on a Cast" />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content="Farcaster Memes Generator" />
-        <meta
-          property="og:description"
-          content="Generate memes based on a Cast"
-        />
-        <meta
-          property="og:image"
-          content="https://infinitememe.lol/INFINITE-MEMES.png"
-        />
-        <meta name="twitter:card" content="player" />
-        <meta name="twitter:site" content="@leo5imon" />
-        <meta name="twitter:title" content="Infinite Memes Generator" />
-        <meta
-          name="twitter:description"
-          content="Generate infinite memes with AI"
-        />
-
-        <meta name="twitter:player" content="https://infinitememes.lol/" />
-
-        <meta name="twitter:player:width" content="360" />
-        <meta name="twitter:player:height" content="560" />
-        <meta
-          name="twitter:image"
-          content="https://infinitememe.lol/INFINITE-MEMES.png"
-        />
-        <meta property="og:url" content="https://infinitememes.lol/farcaster" />
-        <script
-          defer
-          src="https://cloud.umami.is/script.js"
-          data-website-id="4a6ad139-9f43-482c-ab1c-179920236491"
-        ></script>
-      </Head>
       <div className={styles.container}>
         <div className={styles.main}>
           <h1 className={styles.title}>Farcaster Meme Generator ✨</h1>
