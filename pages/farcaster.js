490f33e9a9858df6bb96f0348c01ab72865901c2
leo5imon <leo.simvn@gmail.com>
2024-07-31 12:48:22 +0200

    cast meme

--- pages/farcaster.js
+++ pages/farcaster.js
@@ -1,11 +1,36 @@
 import Cast from "./components/Cast";
 import Footer from "./components/Footer";
+import { Helmet } from "react-helmet";
 import { Analytics } from "@vercel/analytics/react";
 import styles from "./index.module.css";
 
 export default function Farcaster() {
   return (
     <>
+      <Helmet>
+        <title>Farcaster Meme Generator ✨</title>
+        <meta property="og:title" content="Farcaster Meme Generator ✨" />
+        <meta
+          property="og:description"
+          content="Create and share memes with Farcaster Meme Generator."
+        />
+        <meta property="og:url" content="https://yourwebsite.com/farcaster" />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:image"
+          content="https://yourwebsite.com/path/to/your/image.jpg"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Farcaster Meme Generator ✨" />
+        <meta
+          name="twitter:description"
+          content="Create and share memes with Farcaster Meme Generator."
+        />
+        <meta
+          name="twitter:image"
+          content="https://yourwebsite.com/path/to/your/image.jpg"
+        />
+      </Helmet>
       <div className={styles.container}>
         <div className={styles.main}>
           <h1 className={styles.title}>Farcaster Meme Generator ✨</h1>
