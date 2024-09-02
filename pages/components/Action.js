2c608c93865b2b1a4788f0f0e5dca5754f6038fb
leo5imon <leo.simvn@gmail.com>
2024-09-02 12:45:06 +0200

    loading

--- pages/components/Action.js
+++ pages/components/Action.js
@@ -219,6 +219,7 @@ const Action = () => {
                 isLoading ? styles.buttonDisabled : styles.buttonEnabled
               }`}
               disabled={isLoading}
+              data-umami-event={ActionMemeGenerate}
             >
               {isLoading ? "Generating..." : "Generate"}
             </button>
